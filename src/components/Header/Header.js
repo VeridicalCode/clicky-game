ff43792629aea8a219bf2a7bc31d98dbfcad1df7
Mars <m.getsoian@gmail.com>
2020-01-11 00:26:47 -0800

    cleaned up readme and removed dev cheats from images

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,15 +2,22 @@
 // it will stretch across the whole page & have some treble clefs or something
 
 import React from 'react'
+import { useGlobalState } from '../../utils/state';
 
 // takes a prop - the image source - and makes that a tile
 
 function HeaderDiv(props) {
+  const [ globalScore ] = useGlobalState('score');
+  const [ globalTopScore ] = useGlobalState('topScore');
+
   return (
     <div>
-    <h1>Welcome to Instrumental Memory!</h1>
-    <br/>
-    <span>Click the images to increase your score, but don't click the same one twice, or you'll lose!</span>
+      <h1>Welcome to Instrumental Memory!</h1>
+      <br />
+      <span>Click the images to increase your score, but don't click the same one twice, or you'll lose!</span>
+      <span>current score = {globalScore}</span>
+      <br/>
+      <span>top score = {globalTopScore}</span>
     </div>
   );
 }
